33ecd4ffa46bb7a038802d72864a1fc061daf7be
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-10-14 17:56:30 -0300

    --

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const screens = document.querySelectorAll('.screen')
-const ChoseEnemyBtn = document.querySelectorAll('.choose-enemy-btn')
+const ChooseEnemyBtn = document.querySelectorAll('.choose-enemy-btn')
 const startBtn = document.querySelector('#start-btn')
 const gameContainer = document.querySelector('#game-container')
 const timeEl = document.querySelector('.time')
@@ -10,6 +10,25 @@ let seconds = 0
 let score = 0
 let selectEnemy = {}
 
-startBtn.addEventListener('click', () => screens[0].)
+startBtn.addEventListener('click', () => screens[0].classList.add('up'))
 
+ChooseEnemyBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const img = btn.querySelector('img')
+    const src = img.getAttribute('src')
+    const alt = img.getAttribute('alt')
+    selectEnemy = { src, alt }
+    screens[1].classList.add('up')
+    setTimeout(createEnemy, 1000)
+    startGame()
+  })
+})
+
+function createEnemy() {
+  
+}
+
+function startGame() {
+  
+}
 
