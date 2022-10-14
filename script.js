21a61eda8837637f34ff005128cca89646284442
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-10-14 17:45:31 -0300

    --

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const screens = document.querySelectorAll('.screen')
+const ChoseEnemyBtn = document.querySelectorAll('.choose-enemy-btn')
 const startBtn = document.querySelector('#start-btn')
+const gameContainer = document.querySelector('#game-container')
+const timeEl = document.querySelector('.time')
+const scoreEl = document.querySelector('.score')
+const messageEl = document.querySelector('.message')
+
+let seconds = 0
+let score = 0
+let selectEnemy = {}
+
+startBtn.addEventListener('click', () => screens[0].)
+
+
